fe96292ea185fe4606af4ef56294cef924811f29
jaquecantarino <jaquelinecantarino@outlook.com>
2021-07-20 13:50:07 -0300

    modulo 2

---
+++ SQL/estoque.sql
@@ -0,0 +1,11 @@
+-- insert into tb_marcas(nome, ativo) Values ("Nick", true)
+-- select * from tb_marcas
+-- insert into tb_marcas(nome, ativo) Values ("Fatal Surf", false)
+-- select * from tb_marcas
+-- update tb_marcas set nome="Fatal Surf", ativo=true 
+-- where id=2;
+-- select * from tb_marcas
+-- select * from tb_marcas where id <> 2 busca só diferente de 2
+-- select nome from tb_marcas
+-- delete from tb_marcas where id = 3 deleta item
+-- select * from tb_marcas
