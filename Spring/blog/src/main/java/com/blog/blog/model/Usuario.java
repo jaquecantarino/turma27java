be0ed47b9d3d1f174be1629359fdbf8e0f9701a4
jaquecantarino <jaquelinecantarino@outlook.com>
2021-08-09 10:32:15 -0300

    JUnit

--- Spring/blog/src/main/java/com/blog/blog/model/Usuario.java
+++ Spring/blog/src/main/java/com/blog/blog/model/Usuario.java
@@ -1,12 +1,21 @@
 package com.blog.blog.model;
 
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+//import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "tb_usuario")
@@ -25,8 +34,16 @@ public class Usuario {
 	private String usuario;
 	
 	@NotBlank
-	@Size(min=5,max=30)
+	@Size(min=2,max=1000)
 	private String senha;
+	
+	@Column(name = "dt_nascimento")
+	@JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate dataNascimento;
+	
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List <Postagem> postagem;
 
 	public long getId() {
 		return id;
@@ -58,6 +75,22 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 
 }
--- Spring/blog/src/main/java/com/blog/blog/repository/UsuarioRepository.java
+++ Spring/blog/src/main/java/com/blog/blog/repository/UsuarioRepository.java
@@ -1,11 +1,17 @@
 package com.blog.blog.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.blog.blog.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-	public  Optional<Usuario> findByUsuario(String usuario);
+	public Optional <Usuario> findByUsuario(String usuario);
+	
+	public List <Usuario> findAllByNomeContainingIgnoreCase (String nome);
+	
+	public Usuario findByNome(String nome);
+
 }
 
